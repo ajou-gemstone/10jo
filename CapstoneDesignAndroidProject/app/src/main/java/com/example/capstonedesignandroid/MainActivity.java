853798988ea585444d5f6dc5b855b52de7b518ea
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2020-04-06 20:51:57 +0900

    안드로이드 뼈대 작성2

--- CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/MainActivity.java
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.capstonedesignandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button StudyBulletinBoardActivityButton;
+    private Button LectureroomReservationActivityButton;
+    private Button CafeMapActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StudyBulletinBoardActivityButton = findViewById(R.id.StudyBulletinBoardActivityButton);
+        LectureroomReservationActivityButton = findViewById(R.id.LectureroomReservationActivityButton);
+        CafeMapActivityButton = findViewById(R.id.CafeMapActivityButton);
+
+        StudyBulletinBoardActivityButton.setOnClickListener(this);
+        LectureroomReservationActivityButton.setOnClickListener(this);
+        CafeMapActivityButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent activityintent;
+        switch (view.getId()){
+
+            case R.id.StudyBulletinBoardActivityButton:
+                activityintent = new Intent(this, StudyBulletinBoardActivity.class);
+                startActivity(activityintent);
+                break;
+            case R.id.LectureroomReservationActivityButton:
+                activityintent = new Intent(this, LectureroomReservationActivity.class);
+                startActivity(activityintent);
+                break;
+            case R.id.CafeMapActivityButton:
+                activityintent = new Intent(this, CafeMapActivity.class);
+                startActivity(activityintent);
+                break;
+        }
     }
 }
