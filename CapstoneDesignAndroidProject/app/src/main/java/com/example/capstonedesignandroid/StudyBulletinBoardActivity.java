fac0306869bc53f02577ce88201b6b1227d20bf2
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2020-04-06 20:39:42 +0900

    안드로이드 뼈대 작성

---
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/StudyBulletinBoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.capstonedesignandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudyBulletinBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_bulletin_board);
+
+    }
+
+}
