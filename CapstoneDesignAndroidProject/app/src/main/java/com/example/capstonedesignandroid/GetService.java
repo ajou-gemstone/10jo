0b95819c19806efdbfb2690ac5a4fbfbc2cb5cdf
rkdcksgur <rkdcksgur@ajou.ac.kr>
2020-05-05 22:07:45 +0900

    user object 생성

--- CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/GetService.java
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/GetService.java
@@ -1,9 +1,10 @@
 package com.example.capstonedesignandroid;
 
-import com.example.capstonedesignandroid.DTO.Dummy;
+import com.example.capstonedesignandroid.DTO.Group;
 import com.example.capstonedesignandroid.DTO.DummyLectureRoomReservationState;
 import com.example.capstonedesignandroid.DTO.DummyLectureroomInfo;
 import com.example.capstonedesignandroid.DTO.DummyReservationId;
+import com.example.capstonedesignandroid.DTO.User;
 
 import java.util.List;
 
@@ -16,20 +17,18 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GetService {
-    @POST("/study")
-    @FormUrlEncoded
-    Call<List<Dummy3>> listDummies(@Field("id") String position, @Field("pw") String position1);
 
-    @GET("/study/list")
-    Call<List<Dummy3>> listDummies();
-//    Call<List<Dummy3>> listDummies(@Field("id") String position, @Query("position") String position2);
+    @POST("/login")
+    Call<List<User>> postUser(@Field("id") String id, @Field("password") String password);
+
+//    Call<List<Group>> listDummies(@Field("id") String position, @Query("position") String position2);
     //Dummy3의 list형을 POST할 수 있도록 한다.
     //서버에서 req의 body id라는 field에(req.body.id) position이 들어갈 수 있도록 한다.
     //annotation이 붙은 Field나 Query는 server에서 사용하는 key값(변수)이고 오른쪽의 String 변수는 안드로이드에서 사용하는 변수다.
     //Post형은 server에서도 json형 데이터를 쉽게 확인하기 힘들다.
 
     @GET("/study/{position}")
-    Call<List<Dummy3>> listDummies2(@Path("position") String position);
+    Call<List<Group>> listDummies2(@Path("position") String position);
     //Dummy3의 list형을 Get할 수 있도록 한다.
     //서버에서 req의 position이라는 path에(req.params.position) position이 들어갈 수 있도록 한다.
     //Get형은 주소에서 server에서도 json형 데이터를 확인할 수 있다.
