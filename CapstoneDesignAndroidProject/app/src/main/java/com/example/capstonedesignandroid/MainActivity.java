0610b9ef82cca00fce37c308edc91a2edae7a164
rkdcksgur <rkdcksgur@ajou.ac.kr>
2020-05-05 22:06:24 +0900

    main activity navigationview 추가

--- CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/MainActivity.java
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/MainActivity.java
@@ -6,13 +6,23 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.util.Base64;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.kakao.util.maps.helper.Utility;
 
 import org.jsoup.Connection;
@@ -26,8 +36,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import es.dmoral.toasty.Toasty;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    ImageView imageView;
     private Button StudyBulletinBoardActivityButton;
     private Button LectureroomReservationActivityButton;
     private Button CafeMapActivityButton;
@@ -39,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         StudyBulletinBoardActivityButton = findViewById(R.id.StudyBulletinBoardActivityButton);
         LectureroomReservationActivityButton = findViewById(R.id.LectureroomReservationActivityButton);
@@ -53,6 +68,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         testActivity.setOnClickListener(this);
         LectureroomCheckActivityButton.setOnClickListener(this);
         test2Button.setOnClickListener(this);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View nav_header_view = navigationView.getHeaderView(0);
+        TextView user = (TextView) nav_header_view.findViewById(R.id.name);
+        imageView = (ImageView) nav_header_view.findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.profile);
 
         Log.d("asd", getKeyHash(getApplicationContext()));
 
@@ -70,7 +98,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Elements title = doc.select("title");
 
-    }
+
+    } //onCreate
+
+
     public String executeLogin(String id, String pw){
         try {
             Connection.Response loginForm = Jsoup.connect("http://www.naver.com")
@@ -144,17 +175,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
 
+        int id = item.getItemId();
 
+        if (id == R.id.nav_camera) {
 
+        } else if (id == R.id.nav_gallery) {
 
+        } else if (id == R.id.nav_slideshow) {
 
+        } else if (id == R.id.nav_manage) {
 
+        }  else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_question){
 
+        }
+        else if(id == R.id.nav_logout){
 
+        }
+        else if(id == R.id.nav_promise){
 
+        }
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 //    public static String getSigneture(Context context){
 //        PackageManager pm = context.getPackageManager();
