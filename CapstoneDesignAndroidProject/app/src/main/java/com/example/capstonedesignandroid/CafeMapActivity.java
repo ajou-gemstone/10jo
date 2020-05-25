87e89d984a1308534d5b08de635873b12a94bb07
rkdcksgur <rkdcksgur@ajou.ac.kr>
2020-05-25 21:17:17 +0900

    메인 화면 뒤로가기 설정

--- CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/CafeMapActivity.java
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/CafeMapActivity.java
@@ -1,6 +1,8 @@
 package com.example.capstonedesignandroid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -9,6 +11,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.capstonedesignandroid.DTO.DummyCafeCoreInfo;
@@ -125,7 +128,31 @@ public class CafeMapActivity extends AppCompatActivity implements MapView.POIIte
     }
 
     @Override
-    public void onBackPressed() { //super.onBackPressed();비워두면 실행안되서 뒤로가기 안됨
+    public void onBackPressed() {
+        // AlertDialog 빌더를 이용해 종료시 발생시킬 창을 띄운다
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setMessage("종료하시겠습니까?");
+
+        // "예" 버튼을 누르면 실행되는 리스너
+        alBuilder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    finishAffinity();
+                }
+                System.runFinalization();
+                System.exit(0);
+            }
+        });
+        // "아니오" 버튼을 누르면 실행되는 리스너
+        alBuilder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return; // 아무런 작업도 하지 않고 돌아간다
+            }
+        });
+        alBuilder.setTitle("프로그램 종료");
+        alBuilder.show(); // AlertDialog.Bulider로 만든 AlertDialog를 보여준다.
     }
 
     @Override
