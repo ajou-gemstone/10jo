bef8ff29924ac18610b6c4587f03b9936bf334d9
rkdcksgur <rkdcksgur@ajou.ac.kr>
2020-05-26 17:45:42 +0900

    표 ui 개선

--- CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/UserProfileActivity.java
+++ CapstoneDesignAndroidProject/app/src/main/java/com/example/capstonedesignandroid/UserProfileActivity.java
@@ -2,20 +2,26 @@ package com.example.capstonedesignandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.capstonedesignandroid.DTO.User;
+import com.example.capstonedesignandroid.Fragment.TimeTableFragment;
 import com.example.capstonedesignandroid.StaticMethodAndOthers.MyConstants;
 import com.example.capstonedesignandroid.StaticMethodAndOthers.SharedPreference;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -28,6 +34,9 @@ public class UserProfileActivity extends AppCompatActivity {
     ImageView leader, member, waiting;
     String userId;
     protected BottomNavigationView navigationView;
+    private TimeTableFragment timeTableFragment;
+    private RelativeLayout timaTableBigRL;
+    private TimeTableFragment timeTableBigFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +75,45 @@ public class UserProfileActivity extends AppCompatActivity {
         GetService service = retrofit2.create(GetService.class);
         Call<User> call = service.getUserInfo(userId);
         CallThread(call);
+
+        //------------시간표-------------
+        timeTableFragment = new TimeTableFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.timeTableFrame, timeTableFragment).commit();
+
+        //시간표 크게 보기
+        Button timeTableBigButton = findViewById(R.id.timeTableBigButton);
+        timaTableBigRL = findViewById(R.id.timaTableBigRL);
+        timeTableBigButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timaTableBigRL.setVisibility(View.VISIBLE);
+                timeTableBigFragment = new TimeTableFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.timeTableBigFrame, timeTableBigFragment).commit();
+
+                com.otaliastudios.zoom.ZoomLayout timeTableBigZoomLayout = findViewById(R.id.timeTableBigZoomLayout);
+                //UI작업을 위해 handler로 처리, onCreate에서 바로 zoom같은 animation이 작동하지 않는다.
+                Handler mHandler = new Handler();
+                TimerTask mTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    timeTableBigZoomLayout.moveTo(2.5f, 0, 1, true);
+                                }
+                            });
+                        } catch (Exception e) {
+                            Log.d("ExceptionTimer", ": " + e);
+                        }
+                    }
+                };
+
+                Timer mTimer = new Timer();
+                mTimer.schedule(mTask, 200);
+            }
+        });
+
 
 
     }//onCreate
